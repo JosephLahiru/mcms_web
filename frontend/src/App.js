aa160b12729fae29c2bf77ec0b5edaae582cdc86
JamilaHasangi <jamilahasangi02@gmail.com>
2023-01-25 10:55:13 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>this i my fist</h1>
+      <h1>This is my first react project </h1>
     </div>
-  );
+  ); 
 }
 
 export default App;
