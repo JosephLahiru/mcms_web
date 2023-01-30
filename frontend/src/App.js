b8e4db71d05a02b1d8f381e379c0f68d2995c7d2
JamilaHasangi <jamilahasangi02@gmail.com>
2023-01-30 07:07:02 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,11 @@ function App() {
   return (
     <div className="App">
       <h1>Book An Appointment</h1>
+      <h3>Please Select Doctor</h3>
+      <h3>Please Select Appointment</h3>
+      <h3>Please Select Appointment Date</h3>
     </div>
+
   );
 }
 
