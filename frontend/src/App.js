eae23c7e00710176c61bedd45a53623e836f7db1
JamilaHasangi <jamilahasangi02@gmail.com>
2023-02-22 10:26:10 +0530

    appointment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Appointment from './components/Appointment';
 
 function App() {
   return (
     <div className="App">
+      
       <h1>Book An Appointment</h1>
-      <h3>Please Select Doctor</h3>
-      <h3>Please Select Appointment</h3>
-      <h3>Please Select Appointment Date</h3>
+      
+      <Appointment/>
     </div>
+    
 
   );
 }
