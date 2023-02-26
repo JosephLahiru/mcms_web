85099cda9c2636b46bd730b01c2e4490439b26d2
JamilaHasangi <jamilahasangi02@gmail.com>
2023-02-26 19:06:17 +0530

    edit appointmentform

--- frontend/src/components/Appointment.js
+++ frontend/src/components/Appointment.js
@@ -6,7 +6,11 @@ function Appointment() {
   const [name, setName] = useState("");
   const [address, setAddress] = useState("");
   const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
+  const [doctor, setDoctor] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -14,14 +18,18 @@ function Appointment() {
     console.log("Name:", name);
     console.log("Address:",address);
     console.log("Age:", age);
+    console.log("Gender:", gender);
     console.log("Phone number:", phoneNumber);
+    console.log("Doctor:", doctor);
+    console.log("Date:", date);
+    console.log("Time:", time);
   }
 
 
   return (
     <div className="form-container">
     <form onSubmit={handleSubmit}>
-
+    
         <div className="form-input">
             <lable>
           <input type="text" value={appointmentNumber} onChange={(event) => setAppointmentNumber(event.target.value)} placeholder="Enter Appointment Number"/>
@@ -48,7 +56,31 @@ function Appointment() {
       <br />
       <div className="form-input">
       <lable>
+        <input type="text" value={gender} onChange={(event) => setGender(event.target.value) } placeholder="Enter Gender"/>
+        </lable>
+      </div>
+      <br />
+      <div className="form-input">
+      <lable>
         <input type="text" value={phoneNumber} onChange={(event) => setPhoneNumber(event.target.value) } placeholder="Enter Phone number"/>
+        </lable>
+      </div>
+      <br />
+      <div className="form-input">
+      <lable>
+        <input type="text" value={doctor} onChange={(event) => setDoctor(event.target.value) } placeholder=""/>
+        </lable>
+      </div>
+      <br />
+      <div className="form-input">
+      <lable>
+        <input type="text" value={date} onChange={(event) => setDate(event.target.value) } placeholder=""/>
+        </lable>
+      </div>
+      <br />
+      <div className="form-input">
+      <lable>
+        <input type="text" value={time} onChange={(event) => setTime(event.target.value) } placeholder=""/>
         </lable>
       </div>
       <br />
