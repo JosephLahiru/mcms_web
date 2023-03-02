9060d7f62a0a4b7edd86b63a763e830d3df5cc41
PathumLakshitha <pathum_2019513@fot.ruh.ac.lk>
2023-03-02 13:44:48 +0530

    //attendance

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import AddStock from './components/AddStock';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import GetAttendance from './components/GetAttendance';
 
     function App() {
       return (
         <div className="App">
-          <AddStock/>
+          {/* <AddStock/> */}
+          <GetAttendance/>
         </div>
       );
     }
