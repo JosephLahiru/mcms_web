f3b1e4b6101a1668a5e1a8d214a6333fadcfa9d4
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-03-02 12:47:46 +0000

    minor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,7 @@ import Appointment from './components/Appointment.js';
 function App() {
   return (
     <div className="App">
-      
-      <h1>Book An Appointment</h1>
-      
-
-      <Appointment/>
     </div>
-    
-
   );
 }
 
