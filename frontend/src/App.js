f31076972aa00ff982850e137087835085c8e82f
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-03-03 07:05:23 +0000

    fixed minor bugs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import {Routes, Route} from 'react-router-dom';
 import Appointment from './components/Appointment.js';
 import {Login} from "./components/Login";
