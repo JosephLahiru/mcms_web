05e4d5d13ac2d2adf4270ef8d26b8a6dad929fb5
PathumLakshitha <pathum_2019513@fot.ruh.ac.lk>
2023-03-08 21:55:04 +0530

    Input validation for GetAttendance

--- frontend/src/components/GetAttendance.js
+++ frontend/src/components/GetAttendance.js
@@ -6,9 +6,15 @@ function GetAttendance() {
   const [assistantId, setAssistantId] = useState('');
   const [date, setDate] = useState('');
   const [attendanceStatus, setAttendanceStatus] = useState('');
+  const [error, setError] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    if(assistantId.length==0 || date.length==0 ){
+      setError(true);
+    }
+
   };
 
 
@@ -23,33 +29,29 @@ function GetAttendance() {
     <div className="form-container">
     <form onSubmit={handleSubmit}>
         <div className="form-label">
-          <label>
-            Attendance ID
-          </label>
+          <label>Attendance ID</label>
         </div>
         <div className="form-input">
           <input type="text" class="form-control form-control-sm" value={attendanceId} onChange={(event) => setAttendanceId(event.target.value)} />
         </div>
         <div className="form-label">
-          <label>
-            Assistant ID:
-          </label>
+          <label>Assistant ID:</label>
         </div>
         <div className="form-input">
           <input type="text" class="form-control form-control-sm" value={assistantId} onChange={(event) => setAssistantId(event.target.value)} />
         </div>
+        {error&&assistantId.length<=0?
+        <label class='input-validation-error'>Assistant ID can't be Empty</label>:""}
         <div className="form-label">
-          <label>
-            Date:
-          </label>
+          <label>Date:</label>
         </div>
         <div className="form-input">
           <input type="date" class="form-control form-control-sm" value={date} onChange={(event) => setDate(event.target.value)} />
         </div>
+        {error&&date.length<=0?
+        <label class='input-validation-error'>Date can't be Empty</label>:""}
         <div className="form-label">
-          <label>
-            Attendance Status:
-          </label>
+          <label>Attendance Status:</label>
         </div>
         <div className="form-input">
         <select class="form-control form-control-sm" value={attendanceStatus} onChange={(event) => setAttendanceStatus(event.target.value)}>
