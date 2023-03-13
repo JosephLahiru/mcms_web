473e58d81385676e47b4147b1a5c50107e6a25c0
Umeen Rathnayake <umeenbg16@gmail.com>
2023-03-13 12:53:46 +0530

    Dashboard changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
+import { Dashboard } from './components/Dashboard';
 import {Routes, Route} from 'react-router-dom';
 import Appointment from './components/Appointment.js';
 import {Login} from "./components/Login";
 //import AddName from './components/AddName' ;
 import RegistrationForm from './components/RegistrationForm';
-import { Dashboard } from './components/Dashboard';
 
 function App() {
   return (
-      <Routes>
-        <Route path='/' element={<Dashboard/>}/>
-        <Route path='login' element={<Login/>}/>
+      <Routes>   
+        <Route path='/' element={<Login/>}/>
+        <Route path='dashboard' element={<Dashboard/>}/>
         <Route path='appoinment' element={<Appointment/>}/>
         <Route path='registration' element={<RegistrationForm/>}/>
       </Routes>
