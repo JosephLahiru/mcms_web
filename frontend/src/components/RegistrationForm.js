579d72e220b60b8439537df43698e562031e4f9b
Sdilrukshi <73889553+Sdilrukshi@users.noreply.github.com>
2023-03-29 12:17:42 +0530

    change registration form

--- frontend/src/components/RegistrationForm.js
+++ frontend/src/components/RegistrationForm.js
@@ -3,17 +3,17 @@ import './RegistrationForm.css';
 
 export default function RegistrationForm() {
 
-  const [patientType, setPatientType] = useState('');
+  const [patientId, setPatientId] = useState('');
   const [date, setDate] = useState('');
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [nicNumber, setNicNumber] = useState('');
   const [gender, setGender] = useState('');
   const [address, setAddress] = useState('');
-  const [telephoneNumber, setTelephoneNumber] = useState('');
+  const [contactNumber, setContactNumber] = useState('');
 
-  function handlePatientTypeChange(event){
-    setPatientType(event.target.value);
+  function handlePatientIdChange(event){
+    setPatientId(event.target.value);
   }
 
   function handleDateChange(event){
@@ -40,35 +40,35 @@ export default function RegistrationForm() {
     setAddress(event.target.value);
   }
 
-  function handleTelephoneNumberChange(event) {
-    setTelephoneNumber(event.target.value);
+  function handleContactNumberChange(event) {
+    setContactNumber(event.target.value);
   }
 
  
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log('PatientType:', patientType);
+    console.log('PatientId:', patientId);
     console.log('Date:', date);
     console.log('First Name:', firstName);
     console.log('Last Name:', lastName);
     console.log('NIC Number:', nicNumber);
     console.log('Gender:', gender);
     console.log('Address:', address);
-    console.log('Telephone Number:', telephoneNumber);
+    console.log('Contact Number:', contactNumber);
     // You can add code here to submit the form data to a server or perform other actions
   }
 
   function handleCancel(event) {
     event.preventDefault(); // prevent the default form submission behavior
-    setPatientType('');
+    setPatientId('');
     setDate('');
     setFirstName('');
     setLastName('');
     setNicNumber('');
     setGender('');
     setAddress('');
-    setTelephoneNumber('');
+    setContactNumber('');
   }
   
 
@@ -76,68 +76,68 @@ export default function RegistrationForm() {
     <div className='from-container'>
     <form onSubmit={handleSubmit}>
 
-    <div className='row'>
-        <div className='col'>
-            <label>Patient ID:
-                <input type="number" value={date} onChange={handleDateChange} className='form-control' />
-            </label></div>
-        <div className='col'>
-            <label>Appointment Date:
-                <input type="date" value={nicNumber} onChange={handleNicNumberChange} className='form-control' />
-            </label></div></div><br />
+    <div className='form-lable'>
+         <label>Patient Id:</label>
+    </div>
+        <div className='form-input'>      
+          <input type="text" class="form-control form-control-sm" value={patientId} onChange={handlePatientIdChange}/>
+        </div>
 
-      <div className='row'>
-        <div className='col'>
-            <label>NIC Number:
-                <input type="text" value={date} onChange={handleDateChange} className='form-control' />
-            </label></div>
-        <div className='col'>
-            <label>Patient Type:
-            <select value={patientType} onChange={handlePatientTypeChange} className='form-control'>
-                <option value="">Select Patient Type</option>
-                <option value="Type 1">Type 1</option>
-                <option value="Type 2">Type 2</option>
-                <option value="Type 3">Type 3</option>
-                </select>
-            </label></div></div><br />
+        <div className='form-lable'>
+            <label>Appointment Date:</label>
+            </div>
+              <div className='form-input'>
+                <input type="date" class="form-control form-control-sm" value={date} onChange={handleDateChange}/>
+              </div>
 
-      <div className='row'>
-        <div className='col'>
-            <label>First Name:
-                <input type="text" value={firstName} onChange={handleFirstNameChange} className='form-control' />
-            </label></div>
-        <div className='col'>
-            <label>Last Name:
-                <input type="text" value={lastName} onChange={handleLastNameChange} className='form-control'/>
-            </label></div></div><br/>
-        
-        <div className='row'>
-            <div className='col'>    
-            <label>Address:
-        <input type="address" value={address} onChange={handleAddressChange} className='form-control1' />
-      </label></div></div><br />
-      
+        <div className='form-lable'>
+            <label>First Name:</label>
+            </div>
+              <div className='form-input'>
+                <input type="text" class="form-control form-control-sm" value={firstName} onChange={handleFirstNameChange}/>
+                </div>
 
+        <div className='form-lable'>
+            <label>Last Name:</label>
+            </div>
+              <div className='form-input'>
+                <input type="text" class="form-control form-control-sm" value={lastName} onChange={handleLastNameChange}/>
+                </div>
 
+        <div className='form-lable'>
+            <label>NIC Number:</label>
+            </div>
+              <div className='form-input'>
+                <input type="text" class="form-control form-control-sm" value={nicNumber} onChange={handleNicNumberChange}/>
+                </div>
 
-      <div className='row'>
-        <div className='col'>
-            <label>Gender:
-                <select value={gender} onChange={handleGenderChange} className='form-control'>
-                <option value="">Select Gender</option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                <option value="other">Other</option>
-                </select>
-            </label></div>
-        <div className='col'>
-            <label>Telephone Number:
-                <input type="tel" value={telephoneNumber} onChange={handleTelephoneNumberChange} className='form-control'/>
-              </label></div></div><br /><br/>
+        <div className='form-lable'>
+            <label>Gender:</label>
+            </div>
+            <div className='form-input'>
+              <input type="radio" value={gender} checked={gender === "Male"} onChange={handleGenderChange}>Male</input>
+              <input type="radio" value={gender} checked={gender === "Female"} onChange={handleGenderChange}>Female</input>
+            </div>
+
+        <div className='form-lable'>
+            <label>Address:</label>
+            </div>
+              <div className='form-input'>
+                <input type="text" class="form-control form-control-sm" value={address} onChange={handleAddressChange}/>
+              </div>
+
+        <div className='form-lable'>
+            <label>contact Number:</label>
+            </div>
+              <div className='form-input'>
+                <input type="number" class="form-control form-control-sm" value={contactNumber} onChange={handleContactNumberChange}/>
+              </div>
 
 
-      <div><button type="button" onClick={handleCancel}>Cancel</button>
-      <button type="button" onClick={handleSubmit}>Submit</button></div>
+      <div>
+        <button type="button" onClick={handleCancel}>Cancel</button>
+        <button type="button" onClick={handleSubmit}>Submit</button>
+      </div>
 
     </form>
     </div>
