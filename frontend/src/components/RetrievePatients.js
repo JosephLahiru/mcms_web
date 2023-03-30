2eee48dc43b2e6ece29a85c68ef45e6fda6e4cd8
Sdilrukshi <73889553+Sdilrukshi@users.noreply.github.com>
2023-03-30 23:18:22 +0530

    edit the retrieve patients

--- frontend/src/components/RetrievePatients.js
+++ frontend/src/components/RetrievePatients.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function RetrievePatients() {
-  return (
-    <div>
+
+    const [patients, RetrievePatients] = useState([]);
+
+    useEffect(() => {
+        async function fetchPatients() {
+            const response = await fetch("http://158.101.10.103/get_attendance");
+            const data = await response.json();
+            RetrievePatients(data);
+        }
+        fetchPatients();
+    }, []);
+
+return (
+    <div className='content'>
+        <h1>patient List</h1>
+      <table border="1">
+        <tr>
+            <th>Patient Id</th>
+            <th>Appointment date</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>NIC Number</th>
+            <th>Gender</th>
+            <th>Address</th>
+            <th>Contact Number</th>
+        </tr>
+        {patients.map((patients)=>
+        <tr key={patients.patient_id}>
+            <td>{patients.patient_id}</td>
+            <td>{patients.appointment_date}</td>
+            <td>{patients.first_name}</td>
+            <td>{patients.last_name}</td>
+            <td>{patients.nic_number}</td>
+            <td>{patients.gender}</td>
+            <td>{patients.address}</td>
+            <td>{patients.contact_number}</td>
+        </tr>
+        )}
+        </table>
       
     </div>
   )
