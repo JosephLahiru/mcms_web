ff2fd2b74d57d4386ce642390df06dcb9f28dfed
JamilaHasangi <jamilahasangi02@gmail.com>
2023-04-01 22:11:17 +0530

    add udate appointment and patient history

---
+++ frontend/src/components/PatientHistory.js
@@ -0,0 +1,16 @@
+import './PatientHistory.css';
+import './../App.css';
+
+function PatientHistory() {
+
+
+
+return (
+    <div>
+      <h1>Patient History</h1>
+    </div>
+      
+);
+}
+
+export default PatientHistory;
