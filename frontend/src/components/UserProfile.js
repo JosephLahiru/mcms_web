690a84a1162a48cdf3f485be60ffdcd3b1b1b081
Sdilrukshi <73889553+Sdilrukshi@users.noreply.github.com>
2023-04-01 21:12:36 +0530

    coding user  profile

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 export default function UserProfile() {
   return (
-    <div>
-      
-    </div>
+    
+      <div class="container">
+			<div class="row">
+                <div class="column">
+                        <div class="box1"></div>
+                    </div>
+                    <div class="column1">
+                        <div class="box1"></div>
+                    </div>
+                </div>
+	</div>
+
   )
 }
