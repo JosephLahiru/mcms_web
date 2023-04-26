af2b1a7473d3af255c9ce289fed2b67da286201c
JamilaHasangi <jamilahasangi02@gmail.com>
2023-04-26 21:12:23 +0530

    edit view appointment

--- frontend/src/components/ViewAppointment/index.js
+++ frontend/src/components/ViewAppointment/index.js
@@ -25,27 +25,27 @@ function ViewAppointment() {
   };
 
     return (
-        <><><div className="div1">
+        <div className="div1">
         <h1>View Appointment</h1>
         <div className="filter">
           <label htmlFor="dateFilter">Filter by Date:</label>
           <input type="date" id="dateFilter" value={filterDate} onChange={handleFilterDateChange} />
         </div>
-        <label htmlFor="dateFilter">The Universal Physician Doctor:</label>
+         <label className="label1">The Universal Physician:</label>
         <table class="table">
           <thead>
             <tr class="table-dark">
-              <th scope="col"> Appoinment Number </th>
+              <th scope="col"> App Num </th>
               <th scope="col"> First Name </th>
               <th scope="col"> Last Name </th>
               <th scope="col"> NIC </th>
               <th scope="col"> Address </th>
               <th scope="col"> Age </th>
               <th scope="col"> Gender </th>
-              <th scope="col"> Contact Number </th>
+              <th scope="col"> Cont Num </th>
               <th scope="col"> Email </th>
-              <th scope="col"> Appointment Type </th>
-              <th scope="col"> Appointment Time </th>
+              <th scope="col"> App Type </th>
+              <th scope="col"> App Time </th>
             </tr>
           </thead>
           <tbody>
@@ -61,30 +61,26 @@ function ViewAppointment() {
               <td>{appointment.email}</td>
               <td>{appointment.appointmentType}</td>
               <td>{appointment.appointmentTime}</td>
-              <th>
-                <button className="btn btn-primary btn-sm" type="button">Delete</button>
-                <button className="btn btn-primary btn-sm" type="button" onClick={() => navigate("/update_appointment")}>Update Appointment</button>
-              </th>
+              <td><table><tr><td><button className="btn btn-primary btn-sm" type="button">Delete</button></td><td><button className="btn btn-primary btn-sm" type="button" onClick={() => navigate("/update_appointment")}>Update</button></td></tr></table></td>
             </tr>
             )}
           </tbody>
         </table>
-      </div>
-        <label htmlFor="dateFilter">Pediatrician Doctor:</label>
+        <label className="label1">Pediatrician Doctor:</label>
         <table class="table">
           <thead>
             <tr class="table-dark">
-              <th scope="col"> Appoinment Number </th>
+              <th scope="col"> App Num </th>
               <th scope="col"> First Name </th>
               <th scope="col"> Last Name </th>
               <th scope="col"> NIC </th>
               <th scope="col"> Address </th>
               <th scope="col"> Age </th>
               <th scope="col"> Gender </th>
-              <th scope="col"> Contact Number </th>
+              <th scope="col"> Cont Num </th>
               <th scope="col"> Email </th>
-              <th scope="col"> Appointment Type </th>
-              <th scope="col"> Appointment Time </th>
+              <th scope="col"> App Type </th>
+              <th scope="col"> App Time </th>
             </tr>
           </thead>
           <tbody>
@@ -100,27 +96,26 @@ function ViewAppointment() {
               <td>{appointment.email}</td>
               <td>{appointment.appointmentType}</td>
               <td>{appointment.appointmentTime}</td>
-              <th>
-                <button className="btn btn-primary btn-sm" type="button">Delete</button>
-                <button className="btn btn-primary btn-sm" type="button" onClick={() => navigate("/update_appointment")}>Update Appointment</button>
-              </th>
+              <td><table><tr><td><button className="btn btn-primary btn-sm" type="button">Delete</button></td><td><button className="btn btn-primary btn-sm" type="button" onClick={() => navigate("/update_appointment")}>Update</button></td></tr></table></td>
             </tr>
             )}
           </tbody>
-        </table></><label htmlFor="dateFilter">Scan Doctor:</label><table class="table">
+        </table>
+        <label className="label1">Scan Doctor:</label>
+        <table class="table">
           <thead>
             <tr class="table-dark">
-              <th scope="col"> Appoinment Number </th>
+              <th scope="col"> App Num </th>
               <th scope="col"> First Name </th>
               <th scope="col"> Last Name </th>
               <th scope="col"> NIC </th>
               <th scope="col"> Address </th>
               <th scope="col"> Age </th>
               <th scope="col"> Gender </th>
-              <th scope="col"> Contact Number </th>
+              <th scope="col"> Cont Num </th>
               <th scope="col"> Email </th>
-              <th scope="col"> Appointment Type </th>
-              <th scope="col"> Appointment Time </th>
+              <th scope="col"> App Type </th>
+              <th scope="col"> App Time </th>
             </tr>
           </thead>
           <tbody>
@@ -136,11 +131,13 @@ function ViewAppointment() {
               <td>{appointment.email}</td>
               <td>{appointment.appointmentType}</td>
               <td>{appointment.appointmentTime}</td>
-              <td><table><tr><td><button className="btn btn-primary btn-sm" type="button">Delete</button></td><td><button className="btn btn-primary btn-sm" type="button" onClick={() => navigate("/update_appointment")}>Update Appointment</button></td></tr></table>~</td>
+              <td><table><tr><td><button className="btn btn-primary btn-sm" type="button">Delete</button></td><td><button className="btn btn-primary btn-sm" type="button" onClick={() => navigate("/update_appointment")}>Update</button></td></tr></table></td>
             </tr>
-            )}
-          </tbody>
-        </table></>
+           )}
+           </tbody>
+            </table>
+        </div>  
+
 
       );
       }
