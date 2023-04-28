8dae13adb4b349d6992aa71f47d0a4c64570eee0
PathumLakshitha <pathum_2019513@fot.ruh.ac.lk>
2023-04-28 15:44:27 +0530

    changes in view stock

--- frontend/src/components/ViewStock/index.js
+++ frontend/src/components/ViewStock/index.js
@@ -43,9 +43,13 @@ function ViewStock() {
   return (
     <div className="div1">
       <h1>View Stock</h1>
-      <div className="filter">
-        <label htmlFor="drugSearch">Search by <select><option>Drug Name</option><option>Drug type</option><option>Quantity</option></select></label>
-        <input type="text" class="form-control form-control-sm" value={searchTerm} onChange={handleInputChange} placeholder="Search for a drug..."/>
+      <div className="view-stock-filter">
+        <div className="search-filter1">
+          <label className="label3" htmlFor="drugSearch">Search by</label><select><option>Drug Name</option><option>Drug type</option><option>Quantity</option></select>
+        </div>
+        <div className="search-input1">
+          <input type="text" class="form-control form-control-sm" value={searchTerm} onChange={handleInputChange} placeholder="Search for a drug..."/>
+        </div>
       </div>
       <table className="table">
         <thead>
@@ -75,8 +79,8 @@ function ViewStock() {
               <td>{item.ac_price}</td>
               <td>{item.sell_price}</td>
               <td>{item.total_quantity}</td>
-              <td>{item.mfd_date}</td>
-              <td>{item.exp_date}</td>
+              <td>{item.mfd_date.slice(0, 10)}</td>
+              <td>{item.exp_date.slice(0, 10)}</td>
               {/* <td>{item.total_quantity_ac_price}</td>
               <td>{item.total_quantity_sell_price}</td> */}
             </tr>
