2fe5676e6bc5dee8014d9cd564db467974926cc2
Sdilrukshi <73889553+Sdilrukshi@users.noreply.github.com>
2023-04-28 14:45:30 +0530

    add coding profile

--- frontend/src/components/UserProfile/index.js
+++ frontend/src/components/UserProfile/index.js
@@ -4,15 +4,17 @@ import './main.css';
 export default function UserProfile() {
   return (
     
-      <div class="container">
-			<div class="row">
+      <div class="container ">
+        <div className='p_container'>
+			      <div class="row">
                 <div class="column">
                         <div class="box1"></div>
                     </div>
                     <div class="column1">
                         <div class="box1"></div>
                     </div>
-                </div>
+              </div>
+         </div>
 	</div>
 
   )
