27694695c79060763bca2099121549f82460e592
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-05-12 18:47:32 +0000

    fixed issues in index.js (viewendponits)

--- frontend/src/components/ViewEndpoints/index.js
+++ frontend/src/components/ViewEndpoints/index.js
@@ -14,29 +14,32 @@ function App() {
   }, []);
 
   return (
-    <div className="container">
-      <h1 className="title">API Endpoints:</h1>
-      {endpoints ? (
-        <table className="table">
-          <thead>
-            <tr>
-              <th className="header">Endpoint</th>
-              <th className="header">Description</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Object.entries(endpoints).map(([endpoint, description]) => (
-              <tr key={endpoint} className="row">
-                <td className="cell">{endpoint}</td>
-                <td className="cell">{description}</td>
+    <>
+    <br/>
+      <div className="container">
+        <h1 className="title">API Endpoints:</h1>
+        {endpoints ? (
+          <table className="table">
+            <thead>
+              <tr>
+                <th className="header">Endpoint</th>
+                <th className="header">Description</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      ) : (
-        <p className="loading">Loading...</p>
-      )}
-    </div>
+            </thead>
+            <tbody>
+              {Object.entries(endpoints).map(([endpoint, description]) => (
+                <tr key={endpoint}>
+                  <td className="cell">{endpoint}</td><td className="cell">{description}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p className="loading">Loading Please Wait...</p>
+        )}
+      </div>
+      <br/>
+    </>
   );
 }
 
