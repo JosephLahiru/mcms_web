dd61d78f8fd3addbcc71cb4fec9fcbf49b8eaa70
JamilaHasangi <jamilahasangi02@gmail.com>
2023-05-27 11:49:08 +0530

    edit

--- frontend/src/components/UpdateAppointment/index.js
+++ frontend/src/components/UpdateAppointment/index.js
@@ -1,4 +1,4 @@
-import React,{useStatew} from "react";
+import React,{useState} from "react";
 import { ToastContainer, toast } from "react-toastify";
 import 'bootstrap/dist/css/bootstrap.css';
 import "react-toastify/dist/ReactToastify.css";
