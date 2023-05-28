d85db2a5973ab4a301e15466e70b8323a5f9b31b
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-05-28 15:23:12 +0000

    Updated Login to MUI

--- frontend/src/components/Login/index.js
+++ frontend/src/components/Login/index.js
@@ -1,76 +1,127 @@
-import React, { useState } from 'react';
-import './Login.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BsPersonFill, BsLockFill } from 'react-icons/bs';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import bgimage from './../../images/background.png';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {useNavigate} from 'react-router-dom';
 
-const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mcms.mtron.me/">
+        MCMS
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const defaultTheme = createTheme();
+
+export default function SignInSide() {
 
   const navigate = useNavigate();
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (username === 'admin' && password === 'admin') {
-        navigate('/dashboard', {replace:true});
+    const data = new FormData(event.currentTarget);
+    if (data.get('email') === 'admin' && data.get('password') === 'admin') {
+      navigate('/dashboard', {replace:true});
     } else {
       toast.error('Invalid username or password');
     }
   };
 
   return (
-    <div className='login-background'>
-      <div className="login-container">
-        <div className="login-box">
-          <h2 className="login-form-topic">Welcome Back!</h2>
-          <h2 className='login-form-subtopic'>Login to continue.</h2>
-          <form onSubmit={handleSubmit}>
-            <div className="login-form-group">
-              <input
-                type="text"
-                id="username"
-                className="login-frm-control"
-                value={username}
-                onChange={handleUsernameChange}
-                placeholder="Username"
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: '100vh'}}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: `url(${bgimage})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square sx={{backgroundColor:'#f8e7fe'}}>
+          <Box
+            sx={{
+              my: 15,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
                 required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                color="secondary"
               />
-              <BsPersonFill className="login-input-icon" />
-            </div>
-            <div className="login-form-group">
-              <input
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
                 type="password"
                 id="password"
-                className="login-frm-control"
-                value={password}
-                onChange={handlePasswordChange}
-                placeholder="Password"
-                required
+                autoComplete="current-password"
+                color="secondary"
               />
-              <BsLockFill className="login-input-icon" />
-            </div>
-            <center>
-              <button type="submit" className="btn btn-primary login-submit-btn">
-                  Login
-              </button>
-            </center>
-          </form>
-        </div>
-        <ToastContainer />
-      </div>
-    </div>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{
+                  mt: 3,
+                  mb: 2,
+                  bgcolor: 'secondary.main',
+                  ':hover': {
+                    bgcolor: '#d155f6',
+                  },
+                }}
+              >
+                Sign In
+              </Button>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+      <ToastContainer />
+    </ThemeProvider>
   );
-};
-
-export default Login;
+}
