0b50ed899e043e35d2b0d598f30f9191ab6d3da3
Sdilrukshi <73889553+Sdilrukshi@users.noreply.github.com>
2023-05-29 21:09:04 +0530

    change

--- frontend/src/components/History/index.js
+++ frontend/src/components/History/index.js
@@ -1,11 +1,13 @@
 import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import React, { useState } from 'react';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import Button from '@mui/material/Button';
 
 
 
@@ -17,6 +19,8 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
+
+
 export default function BasicGrid() {
   const [drugId, setDrugId] = useState('');
   const [drugName, setDrugName] = useState('');
@@ -25,7 +29,33 @@ export default function BasicGrid() {
   const [discount, setDiscount] = useState(0);
   const [invDate, setInvDate] = useState('');
   const [appointmentId, setAppointmentId] = useState('');
+  const [invNum, setInvNum] = useState('');
+  const [doctor, setDoctor] = useState('');
+  const [doctorCharge, setDoctorCharge] = useState('');
+  const [open, setOpen] = useState(false);
 
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const formData = {
+    drugId,
+    drugName,
+    quantity,
+    total,
+    discount,
+    invDate,
+    appointmentId,
+    invNum,
+    doctor,
+    doctorCharge,
+  };
+
+  console.log('Form Data:', formData);
 
 const handleReset = () => {
     setDrugId('');
@@ -33,6 +63,8 @@ const handleReset = () => {
     setQuantity(0);
     setTotal(0);
     setDiscount(0);
+    setInvNum(0);
+    setDoctor('');
   };
 
   const handleSubmit = (e) => {
@@ -48,19 +80,17 @@ const handleReset = () => {
     console.log('Total:', total);
     console.log('Discount:', discount);
     console.log('Discounted Total:', discountedTotal);
+    console.log('Invoice Number:',invNum);
 
     // Reset form after submission
     handleReset();
-
   
-
   };
   return (
     <React.Fragment>
     <CssBaseline />
-    <Container fixed>
-    <Box sx={{  backgroundColor:'#CCCCFF', height:'100vh',padding:'20px', width:'700px'}}>
-      <Grid container spacing={0.5}>
+    <Container fixed sx={{  backgroundColor:'#CCCCFF', height:'100vh',padding:'20px', alignItems:'stretch'}}>
+      <Grid container spacing={0.5} sx={{ alignItems:'stretch'}}>
       <Grid item xs={12}>
           <Item>
           <h3>K.G.N. Medi house Invoice</h3>
@@ -69,77 +99,184 @@ const handleReset = () => {
           Fax: +94 20 7496 4630.
           </Item>
         </Grid>
-        <Grid item xs={4}>
+
+        <Grid item xs={6} sx={{ height: '300px' }}>
           <Item>
-          <form onSubmit={handleSubmit}>
-          <div style={{marginBottom:'20px'}}>
-          <label htmlFor="Date" style={{ width:'150px'}}>Inv/Date:</label>
-          <input type="date" id="inv/date" value={invDate} style={{width:'150px'}} onChange={(e) => setInvDate(e.target.value)}required/>
-        </div>
-        <div style={{marginBottom:'20px'}}>
-          <label htmlFor="Date" style={{ width:'150px'}}>Inv/Date:</label>
-          <input type="time" id="inv/date" value={invDate} style={{width:'150px'}}  onChange={(e) => setInvDate(e.target.value)}required/>
-        </div>
-        </form>  
-      </Item>
+          <FormControl fullWidth sx={{ paddingBottom: '1px', marginLeft:'10px', width:'400px', marginRight:'10px'}}  onSubmit={handleSubmit}>
+          <label>Invoice Date</label>
+            <TextField id="outlined-date" 
+            label="Inv/Date" 
+            type="date" 
+            alue={invDate}
+            onChange={(e) => setInvDate(e.target.value)}
+            defaultValue="Small"
+            size="small"
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+            shrink: true,}}/>
+          </FormControl>
+          <br/>
+
+          <FormControl fullWidth sx={{ paddingBottom: '1px', marginLeft:'10px', width:'400px', marginRight:'10px'}}  onSubmit={handleSubmit}>
+            <label>Invoice Number</label>
+          <TextField id="outlined-number" 
+          label="Inv/Number" 
+          type="number" 
+          alue={invNum}
+          onChange={(e) => setInvNum(e.target.value)}
+          defaultValue="Small"
+          size="small"
+          margin="normal"
+          fullWidth
+          InputLabelProps={{
+            shrink: true,}}/>
+         
+          </FormControl>
+     
+        <FormControl fullWidth sx={{  paddingBottom: '6px', marginLeft:'10px', width:'400px', marginRight:'10px', marginBottom:'10px'}} onSubmit={handleSubmit}>
+        <label>Appointment Number</label>
+        <TextField
+          id="outlined"
+          label="App/ID:"
+          value={appointmentId}
+          onChange={(e) => setAppointmentId(e.target.value)}
+          required
+          fullWidth
+          margin="normal"
+          defaultValue="Small"
+          size="small"/>
+      </FormControl><br/>
+
+ 
+
+      <FormControl fullWidth sx={{   marginLeft:'10px', width:'400px', marginRight:'10px', marginBottom:'10px'}} onSubmit={handleSubmit}>
+      <label>Select Doctor</label>
+      <InputLabel id="outlined-demo-controlled-open-select-label">Select Doctor</InputLabel>
+      <Select
+    labelId="demo-controlled-open-select-label"
+     open={open}
+    onClose={handleClose}
+    onOpen={handleOpen}
+    id="outlined"
+    value={doctor}
+    onChange={(e) => setDoctor(e.target.value)}
+    required
+    fullWidth
+    margin="normal"
+    size="small"
+ 
+>
+    <MenuItem value="doctor1">Doctor 1</MenuItem>
+    <MenuItem value="doctor2">Doctor 2</MenuItem>
+    <MenuItem value="doctor3">Doctor 3</MenuItem>
+    <MenuItem value="doctor4">Doctor 4</MenuItem>
+  </Select>
+      </FormControl>
+
+      <FormControl fullWidth sx={{  marginLeft:'10px', width:'400px', marginRight:'10px', marginBottom:'10px'}} onSubmit={handleSubmit}>
+      <label>Doctor Charge</label>
+        <TextField
+          id="outlined-doctor-charge"
+          label="Doctor Charge:"
+          type='number'
+          value={doctorCharge}
+          onChange={(e) => setDoctorCharge(e.target.value)}
+          required
+          fullWidth
+          margin="normal"
+          defaultValue="Small"
+          size="small"
+          inputProps={{
+          step: '0.01',
+          min: '0',
+        }}
+        />
+      </FormControl></Item>
         </Grid>
-        <Grid item xs={8}>
+
+        <Grid item xs={6}>
           <Item>
-          <form onSubmit={handleSubmit}>
-          <div style={{marginBottom:'20px'}}>
-          <label htmlFor="appointmentId" style={{marginRight:'50px', width:'150px'}}>Appontment ID:</label>
-          <input type="text" id="appointmentId" value={appointmentId} onChange={(e) => setAppointmentId(e.target.value)}required/>
-        </div>
+  <FormControl fullWidth sx={{  marginLeft:'10px', width:'400px', marginRight:'10px', marginBottom:'10px'}} onSubmit={handleSubmit}>
+        <TextField
+          id="drugId"
+          label="Drug ID:"
+          value={drugId}
+          onChange={(e) => setDrugId(e.target.value)}
+          required
+          margin="normal"
+          size="small"
+        />
+        </FormControl>
 
-        <div style={{marginBottom:'20px'}}>
-          <label htmlFor="appointmentId" style={{marginRight:'50px', width:'150px'}}>Appontment ID:</label>
-          <input type="text" id="appointmentId" value={appointmentId} onChange={(e) => setAppointmentId(e.target.value)}required/>
-        </div>
-
-        <div style={{marginBottom:'20px'}}>
-          <label htmlFor="appointmentId" style={{marginRight:'50px', width:'150px'}}>Appontment ID:</label>
-          <input type="text" id="appointmentId" value={appointmentId} onChange={(e) => setAppointmentId(e.target.value)}required/>
-        </div>
-            </form></Item></Grid>
-        <Grid item xs={8}>
-          <Item> <form onSubmit={handleSubmit}>
-        <div style={{marginBottom:'20px'}}>
-          <label htmlFor="drugId" style={{marginRight:'50px', width:'100px'}}>Drug ID:</label>
-          <input type="text" id="drugId" value={drugId} onChange={(e) => setDrugId(e.target.value)}required/>
-        </div>
-        <div style={{marginBottom:'20px'}}>
-          <label htmlFor="drugName" style={{marginRight:'50px',width:'100px'}}>Drug Name:</label>
-          <input type="text" id="drugName" value={drugName} onChange={(e) => setDrugName(e.target.value)}required/>
-        </div>
-        <div style={{marginBottom:'20px'}}>
-          <label htmlFor="quantity" style={{width:'100px',marginRight:'50px'}}>Quantity:</label>
-          <input type="number" id="quantity" value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}required/>
-        </div>
-        <div style={{marginBottom:'20px'}}>
-          <label htmlFor="total" style={{marginRight:'50px',width:'100px'}}>Total:</label>
-          <input type="number" id="total" value={total} onChange={(e) => setTotal(Number(e.target.value))}required/>
-        </div>
-        <div style={{marginBottom:'20px'}}>
-          <label htmlFor="discount" style={{marginRight:'50px',width:'100px'}}>Discount(%):</label>
-          <input type="number" id="discount" value={discount}onChange={(e) => setDiscount(Number(e.target.value))}required/>
-        </div>
-        <div>
-        <Stack spacing={1} direction="row">
-          <button variant="contained" color='success'>Submit</button>
-          <button variant="button" onClick={handleReset}>Reset</button>
+     <FormControl fullWidth sx={{  marginLeft:'10px', width:'400px', marginRight:'10px', marginBottom:'10px'}} onSubmit={handleSubmit}>
+        <Select
+          id="drugName"
+          label="Drug Name:"
+          value={drugName}
+          onChange={(e) => setDrugName(e.target.value)}
+          required
+          margin="normal"
+          size="small"
+        >
+    
+    <MenuItem value="drug 1">Drug 1</MenuItem>
+    <MenuItem value="drug 2">Drug 2</MenuItem>
+    <MenuItem value="drug 3">Drug 3</MenuItem>
+    <MenuItem value="drug 4">Drug 4</MenuItem>
+    <MenuItem value="drug 5">Drug 5</MenuItem>
+  </Select>
+        </FormControl>
+      
+      <FormControl fullWidth sx={{  marginLeft:'10px', width:'400px', marginRight:'10px', marginBottom:'10px'}} onSubmit={handleSubmit}>
+        <TextField
+          id="quantity"
+          label="Quantity:"
+          type="number"
+          value={quantity}
+          onChange={(e) => setQuantity(Number(e.target.value))}
+          required
+          margin="normal"
+          size="small"
+             inputProps={{
+          step: '1',
+          min: '0',
+        }}
+        />
+        </FormControl>
+   
+   <FormControl fullWidth sx={{  marginLeft:'10px', width:'400px', marginRight:'10px', marginBottom:'10px'}} onSubmit={handleSubmit}>
+        <TextField
+          id="total"
+          label="Total:"
+          type="number"
+          value={total}
+          onChange={(e) => setTotal(Number(e.target.value))}
+          required
+          margin="normal"
+          size="small"
+        />
+        </FormControl>
+      
+      <FormControl fullWidth sx={{  marginLeft:'10px', width:'400px', marginRight:'10px', marginBottom:'10px'}} onSubmit={handleSubmit}>
+        <TextField
+          id="discount"
+          label="Discount(%):"
+          type="number"
+          value={discount}
+          onChange={(e) => setDiscount(Number(e.target.value))}
+          required
+          margin="normal"
+          size="small"
+        /></FormControl>
+     
+          <Stack spacing={2} direction="row" sx={{paddingLeft:'100px', paddingTop:'60px'}}>
+          <Button variant="contained" color='success'>Submit</Button>
+          <Button variant="contained" color='error' onClick={handleReset}>Reset</Button>
           </Stack>
-        </div>
-      </form></Item>
+          </Item>
+          </Grid>
         </Grid>
-        
-        <Grid item xs={4}>
-          <Item>xs=4</Item>
-        </Grid>
-        <Grid item xs={8}>
-          <Item>xs=8</Item>
-        </Grid>
-      </Grid>
-    </Box>
     </Container>
     </React.Fragment>
   );
