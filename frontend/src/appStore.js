9e9154f9135240a2bd5a9c845d6efcf59eeb16cc
Umeen Rathnayake <umeenbg16@gmail.com>
2023-05-29 10:53:17 +0530

    Added sidenav, navbar and appStore. Removed css

---
+++ frontend/src/appStore.js
@@ -0,0 +1,12 @@
+import create from "zustand"
+import {persist} from "zustand/middleware"
+
+
+let appStore = (set) => ({
+    dopen: true,
+    updateOpen:(dopen) => set((state) => ({dopen:dopen})),
+
+});
+
+appStore = persist(appStore, {name: "my_app_store"});
+export const useAppstore = create(appStore);
