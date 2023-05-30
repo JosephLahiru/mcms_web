6066502a515489576c1e0eb682cf2e6e06a3594f
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-05-30 23:51:16 +0530

    Updated Dashboard

--- frontend/src/components/Dashboard/index.js
+++ frontend/src/components/Dashboard/index.js
@@ -1,27 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Navbar from './../Navbar';
+import SideBar from './../SideBar';
+import WelcomePage from '../WelcomePage';
 
 const Dashboard = () => {
   return (
-    <div className="dashboard-container">
-      <div className="dashboard-content">
-        <h1>Dashboard</h1>
-        <div className="button-container">
-          <Link to="/add-stock">
-            <button className="dashboard-button">Add Stock</button>
-          </Link>
-          <Link to="/delete-stock">
-            <button className="dashboard-button">Delete Stock</button>
-          </Link>
-          <Link to="/add-appointment">
-            <button className="dashboard-button">Add Appointment</button>
-          </Link>
-          <Link to="/view-stock">
-            <button className="dashboard-button">View Stock</button>
-          </Link>  
-        </div>
-      </div>
-    </div>
+    <>
+      <Navbar/>
+      <SideBar/>
+      <WelcomePage/>
+    </>
   );
 };
 
