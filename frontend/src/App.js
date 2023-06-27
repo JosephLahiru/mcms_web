af8131d0f08b2488a63c34769e954c40cd759223
JamilaHasangi <jamilahasangi02@gmail.com>
2023-06-27 23:05:44 +0530

    edit add appointment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import ViewAttendance from './components/ViewAttendance';
 import UpdateStock from './components/UpdateStock';
 import ViewStock from './components/ViewStock';
 import AddAppointment from './components/AddAppoinment';
+import AddAppointment1 from './components/AddAppoinment1';
 import ViewLowStock from './components/ViewLowStock';
 import ViewShortExpiry from './components/ViewShortExpiry';
 import Navbar from './components/Navbar';
@@ -35,6 +36,7 @@ function App() {
         <Route path='/' element={<Login />} />
         <Route path='dashboard' element={<Dashboard />} />
         <Route path='add_appointment' element={<AddAppointment />} />
+        <Route path='add_appointment1' element={<AddAppointment1 />} />
         <Route path='view_appointment' element={<ViewAppointment />} />
         <Route path='update_appointment' element={<UpdateAppointment />} />
         <Route path='patient_history' element={<PatientHistory />} />
---
+++ frontend/src/components/AddAppoinment1/index.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import 'react-toastify/dist/ReactToastify.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+import { 
+  Grid, 
+  Box,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Alert,
+  Stack,
+  AlertTitle,
+   } from '@mui/material';
+
+  function AddAppointment1(){
+    const [doctor, setDoctor] = useState('');
+
+    const handleOptionChange = (event) => {
+      setDoctor(event.target.value);
+    };
+
+    return (
+    <Grid>
+    <Box sx={{width: 2000,height: 250,backgroundColor: '#ce93d8', }}>
+      <Grid item xs={12} sx={{paddingTop:'130px', paddingBottom:'50px'}}>
+        <Grid container spacing={0} alignItems="center" justifyContent="center" >
+          <Grid item  sx={{backgroundColor : 'white'}} >
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">SELECT A DOCTOR</InputLabel>
+              <Select labelId="demo-simple-select-label" id="demo-simple-select" value={doctor} onChange={handleOptionChange} sx={{width: '500px'}} label = "SELECT A DOCTOR" >
+                <MenuItem value="option1">Universal Physician</MenuItem>
+                <MenuItem value="option2">Pediatrician</MenuItem>
+                <MenuItem value="option3">Radiologist</MenuItem>
+              </Select>
+            </FormControl> 
+          </Grid> 
+        </Grid>
+        </Grid>   
+      </Box>
+      <Stack  sx={{ width: '115%',paddingTop:'70px' }}  spacing={0}  alignItems="center" justifyContent="center">
+      <Alert severity="info">
+        <AlertTitle>Info</AlertTitle>
+            Please Select Doctor — <strong>CHECK IT OUT!!</strong>
+      </Alert>
+      </Stack>
+      </Grid>
+
+    );
+}
+
+export default AddAppointment1;   
