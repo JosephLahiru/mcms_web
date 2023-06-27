ec1c2b1102bec8c728ddb848fb902ce06633ff73
JamilaHasangi <jamilahasangi02@gmail.com>
2023-06-27 17:35:51 +0530

    edit

--- frontend/src/components/AddAppoinment/index.js
+++ frontend/src/components/AddAppoinment/index.js
@@ -2,24 +2,20 @@ import React, { useState } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
+
 import { 
   Grid, 
-  Select, 
-  MenuItem, 
-  FormControl, 
-  InputLabel, 
-  Container,
   Box,
    } from '@mui/material';
 
   function AddAppointment(){
-  return (
-    <React.Fragment>
-      <Container maxWidth="sm">
-        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
-      </Container>
-    </React.Fragment>
-  );
+
+    return (
+      <Box sx={{width: 2000,height: 200,backgroundColor:'#e040fb', }}>
+        
+      </Box>
+      
+    );
 }
 
 export default AddAppointment;   
