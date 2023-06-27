79498e8f35842729fa85b7f9d209c2c9e12fd594
JamilaHasangi <jamilahasangi02@gmail.com>
2023-06-27 19:03:43 +0530

    edit add appointment

--- frontend/src/components/AddAppoinment/index.js
+++ frontend/src/components/AddAppoinment/index.js
@@ -6,13 +6,49 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { 
   Grid, 
   Box,
+  TextField,
+  Button,
+  Select,
+  MenuItem,
    } from '@mui/material';
 
   function AddAppointment(){
+    const [selectedOption, setSelectedOption] = useState('');
+    const [formData, setFormData] = useState({
+    
+    });
 
+    const handleOptionChange = (event) => {
+      setSelectedOption(event.target.value);
+    };
+  
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      // Perform form submission logic here
+      console.log(formData);
+    };
     return (
-      <Box sx={{width: 2000,height: 200,backgroundColor:'#e040fb', }}>
-        
+      <Box sx={{width: 2000,height: 1000,backgroundColor: '#ce93d8', }}>
+          <form onSubmit={handleSubmit} >
+          <Grid item xs={12} sx={{paddingTop:'200px', paddingBottom:'60px'}}>
+        <Grid container spacing={1} alignItems="center" justifyContent="center">
+          <Grid item sx={{backgroundColor : 'white'}}>
+            <Select value={selectedOption} onChange={handleOptionChange}>
+              <MenuItem value="option1">Universal Physician</MenuItem>
+              <MenuItem value="option2">Pediatrician</MenuItem>
+              <MenuItem value="option3">Radiologist</MenuItem>
+            </Select>
+          </Grid>
+          <Grid item>
+            <Button type="submit" variant="contained" color="primary">
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
+        </Grid>
+      </form>
+     
       </Box>
       
     );
