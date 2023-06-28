e08bbf35b59cc4ed2a5d0374e318d3d47a3491e1
JamilaHasangi <jamilahasangi02@gmail.com>
2023-06-28 22:40:27 +0530

    add new folder for add appointment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import UpdateStock from './components/UpdateStock';
 import ViewStock from './components/ViewStock';
 import AddAppointment from './components/AddAppoinment';
 import AddAppointment1 from './components/AddAppoinment1';
+import AddAppointment2 from './components/AddAppoinment2';
 import ViewLowStock from './components/ViewLowStock';
 import ViewShortExpiry from './components/ViewShortExpiry';
 import Navbar from './components/Navbar';
@@ -37,6 +38,7 @@ function App() {
         <Route path='dashboard' element={<Dashboard />} />
         <Route path='add_appointment' element={<AddAppointment />} />
         <Route path='add_appointment1' element={<AddAppointment1 />} />
+        <Route path='add_appointment2' element={<AddAppointment2 />} />
         <Route path='view_appointment' element={<ViewAppointment />} />
         <Route path='update_appointment' element={<UpdateAppointment />} />
         <Route path='patient_history' element={<PatientHistory />} />
