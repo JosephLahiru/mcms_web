87289d865f6a79c0de9ddbc3d7625eafcfbf7165
JamilaHasangi <jamilahasangi02@gmail.com>
2023-06-28 22:52:56 +0530

    edit add appointment

--- frontend/src/components/AddAppoinment/index.js
+++ frontend/src/components/AddAppoinment/index.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-
 import { 
   Grid, 
   Box,
---
+++ frontend/src/components/AddAppoinment2/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+
+import { 
+    Grid, 
+    Box,
+    Select,
+     } from '@mui/material';
+
+     function AddAppointment2(){
+     
+
+
+    return (
+        <Grid container spacing={0}>
+        <Grid item xs={12}>
+        <Box sx={{width: '100%',height: 250,backgroundColor: '#ce93d8', }}>
+
+        </Box>
+        </Grid>
+        </Grid>
+        
+
+        );
+    }
+    
+    export default AddAppointment2;   
