56c43e325c6c6c00152adf9630506e4a825dc798
JamilaHasangi <jamilahasangi02@gmail.com>
2023-07-02 10:40:15 +0530

    edit update appointment

--- frontend/src/components/UpdateAppointment/index.js
+++ frontend/src/components/UpdateAppointment/index.js
@@ -28,10 +28,11 @@ function UpdateAppointment() {
 
   return (
     <Grid item xs={12}>
-    <Box sx={{ width: '100%', height: 175, backgroundColor: '#ce93d8' }}>
+    <Box sx={{ width: '100%', height: 100, backgroundColor: '#ce93d8' }}>
       <Typography variant="h4" component="div" sx={{ color: 'white', fontWeight: 'bold', paddingTop: '50px', textAlign: 'left', paddingLeft: '90px' }}>
-        BOOK A CHANNEL
+        UPDATE INFORMATION
       </Typography>
+      <CloseOutlinedIcon sx={{ position: 'absolute', top: '80px', right: '20px', color: 'white' }} />
     </Box>
   </Grid>
   );
