e4ef10ac93f11765736a22f79de343e38faf115a
JamilaHasangi <jamilahasangi02@gmail.com>
2023-07-02 15:46:50 +0530

    edit update appointment

--- frontend/src/components/UpdateAppointment/index.js
+++ frontend/src/components/UpdateAppointment/index.js
@@ -6,17 +6,10 @@ import {
   Grid,
   Box, 
   Typography,
-  handleClose,
-  style,
-  Divider,
-  TextField,
-  RadioGroup,
-  FormControlLabel,
-  Radio,
-  Button,
-  Modal,
-  handleOpen,
-  open,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
 
 
 
@@ -58,167 +51,55 @@ function UpdateAppointment() {
 
 
   return (
-    <Grid container spacing={3}>
+    <Grid container spacing={2}>
     <Grid item xs={12}>
       <Box sx={{ width: '100%', height: 100, backgroundColor: '#ce93d8' }}>
-        <Typography variant="h4" component="div" sx={{ color: 'white', fontWeight: 'bold', paddingTop: '50px', textAlign: 'left', paddingLeft: '90px' }}>
+        <Typography variant="h4" component="div" sx={{ color: 'white', fontWeight: 'bold', paddingTop: '40px', textAlign: 'left', paddingLeft: '90px' }}>
           UPDATE INFORMATION
         </Typography>
         <CloseOutlinedIcon sx={{ position: 'absolute', top: '80px', right: '20px', color: 'white' }} />
       </Box>
     </Grid>
     <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
-      <Box sx={{ width: '1200px', height: 170, backgroundColor: '#f5f5f5', borderRadius: '10px', display: 'flex', alignItems: 'center' }}>
-      <Grid item xs={12} >
-      <Box sx={{ width: '100%', height: 5 ,marginBottom: '150px'}}>
-        <Typography  component="div" sx={{ color: 'purple', fontWeight: 'bold', paddingTop: '20px', textAlign: 'center',fontSize: '40px' }}>
+      <Box sx={{ width: '1200px', height: 675, backgroundColor: '#f5f5f5', borderRadius: '10px' }}>
+        <Typography  component="div" sx={{ color: 'purple', fontWeight: 'bold', textAlign: 'center',fontSize: '40px',paddingBottom:'15px' }}>
           Appointment Information
-        </Typography>  
+        </Typography> 
+      <Grid container spacing={0}>
+    <Grid item xs={4}>
+    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label" color="secondary">SELECT A SPECIALITY</InputLabel>
+                            <Select labelId="demo-simple-select-label" color="secondary" id="demo-simple-select" value={appointmentDoctor}   sx={{width: '500px'}} label = "SELECT A DOCTOR" >
+                            <MenuItem value="option1">Universal Physician - NISHANTHA GUNASEKARA</MenuItem>
+                            <MenuItem value="option2">Pediatrician - BUDDHI MOHOTTI</MenuItem>
+                            <MenuItem value="option3">Radiologist - PRESANTHA BANDARA</MenuItem>
+                            </Select>
+                    </FormControl>
+    </Grid>
+    <Grid item xs={4}>
+    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label" color="secondary">SELECT A SPECIALITY</InputLabel>
+                            <Select labelId="demo-simple-select-label" color="secondary" id="demo-simple-select" value={appointmentDoctor}   sx={{width: '500px'}} label = "SELECT A DOCTOR" >
+                            <MenuItem value="option1">Universal Physician - NISHANTHA GUNASEKARA</MenuItem>
+                            <MenuItem value="option2">Pediatrician - BUDDHI MOHOTTI</MenuItem>
+                            <MenuItem value="option3">Radiologist - PRESANTHA BANDARA</MenuItem>
+                            </Select>
+                    </FormControl>
+    </Grid>
+    <Grid item xs={4}>
+    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label" color="secondary">SELECT A SPECIALITY</InputLabel>
+                            <Select labelId="demo-simple-select-label" color="secondary" id="demo-simple-select" value={appointmentDoctor}   sx={{width: '500px'}} label = "SELECT A DOCTOR" >
+                            <MenuItem value="option1">Universal Physician - NISHANTHA GUNASEKARA</MenuItem>
+                            <MenuItem value="option2">Pediatrician - BUDDHI MOHOTTI</MenuItem>
+                            <MenuItem value="option3">Radiologist - PRESANTHA BANDARA</MenuItem>
+                            </Select>
+                    </FormControl>
+    </Grid>
+  </Grid>
       </Box>
       </Grid>
-      <Grid item xs={12} >
-      <Grid item xs={12} sm={12} container spacing={8} >
-        <Grid item xs={4}  sx={{ display: 'flex', justifyContent: 'left' }}>
-          <TextField
-           id="age"
-           label="Patient Age"
-           value={age}
-           onChange={(event) => setAge(event.target.value)}
-           variant="outlined"
-           color="secondary"
-           sx={{ width: '90%' , marginBottom: '20px'}}
-         /> 
-        </Grid>
-        <Grid item xs={4} sx={{ display: 'flex', justifyContent: 'center' }} >
-          <TextField 
-           id="mobile"
-           label="Patient Mobile"
-           value={mobile}
-           onChange={(event) => setMobile(event.target.value)}
-           variant="outlined"
-           color="secondary"
-           sx={{ width: '90%' , marginBottom: '20px'}}
-         />
-        </Grid>
-        <Grid item xs={4} sx={{ display: 'flex', justifyContent: 'right' }} >
-          <TextField 
-           id="mobile"
-           label="Patient Mobile"
-           value={mobile}
-           onChange={(event) => setMobile(event.target.value)}
-           variant="outlined"
-           color="secondary"
-           sx={{ width: '90%' , marginBottom: '20px'}}
-         />
-        </Grid>
-        </Grid>
-        </Grid>
-      </Box>
-    </Grid>
-    <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
-      <Box sx={{ width: '1200px', height: 470, backgroundColor: '#f5f5f5', borderRadius: '10px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <Grid container spacing={0}  >
-      <Grid item xs={12} >
-      <Box sx={{ width: '100%', height: 5 ,marginBottom: '80px'}}>
-        <Typography  component="div" sx={{ color: 'purple', fontWeight: 'bold', paddingTop: '20px', textAlign: 'center',fontSize: '40px' }}>
-          Patient Information
-        </Typography>  
-      </Box>
-          </Grid>
-      <Grid  item xs={12} sx={{ display: 'flex', justifyContent: 'center' }} >
-        <TextField
-          id="patient-name"
-          label="Patient Name"
-          value={patientName}
-          onChange={(event) => setPatientName(event.target.value)}
-          variant="outlined"
-          color="secondary"
-          sx={{ width: '90%' , marginBottom: '20px',marginTop: '10px'}}
-        />
-          </Grid>
-        <Grid item xs={12} sm={12} container spacing={8} >
-        <Grid item xs={6}  sx={{ display: 'flex', justifyContent: 'right' }}>
-          <TextField
-           id="age"
-           label="Patient Age"
-           value={age}
-           onChange={(event) => setAge(event.target.value)}
-           variant="outlined"
-           color="secondary"
-           sx={{ width: '90%' , marginBottom: '10px'}}
-         /> 
-        </Grid>
-        <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'left' }} >
-          <TextField 
-           id="mobile"
-           label="Patient Mobile"
-           value={mobile}
-           onChange={(event) => setMobile(event.target.value)}
-           variant="outlined"
-           color="secondary"
-           sx={{ width: '90%' , marginBottom: '10px'}}
-         />
-        </Grid>
-        </Grid>
-          <Grid item xs={12}  sx={{ display: 'flex', justifyContent: 'center', marginBottom: '10px' }}  >
-          <RadioGroup
-      row
-      aria-labelledby="demo-row-radio-buttons-group-label"
-      name="row-radio-buttons-group"
-      value={gender} 
-      onChange={(event) => setGender(event.target.value)}
-      sx={{ width: '90%',marginBottom: '10px' }}
-
-    >
-      <FormControlLabel value="female" control={<Radio />} label="Female"  sx={{ marginRight: '100px' }}/>
-      <FormControlLabel value="male" control={<Radio />} label="Male"  />
-    </RadioGroup>
-        </Grid>
-        <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }} >
-        <TextField
-          id="area"
-          label=" Patient Area"
-          value={area}
-          onChange={(event) => setArea(event.target.value)}
-          variant="outlined"
-          color="secondary"
-          sx={{ width: '90%' , marginBottom: '20px'}}
-        />
-          </Grid>
-          <Grid item xs={12}  sx={{ display: 'flex', justifyContent: 'center'}} >
-          <Button variant="contained" size="medium" color="secondary" sx={{ width: '1075px', height: '50px',fontSize: '24px' }} onClick={handleOpen}Open modal>Update Now</Button>
-          <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="Patient Information Title"
-                aria-describedby="Patient Information Description "
-              >
-                <Box sx={style}>
-                  <Typography id="Patient Information Title" variant="h6" component="h2" sx={{ color: 'black', fontWeight: 'bold',fontSize: '24px'}}>
-                  Patient Information Title
-                  </Typography>
-                  <Typography id="Patient Information Description" sx={{ mt: 2 ,fontWeight: 'bold' }}>
-                    NAME: {patientName ? `${patientName}` : ''} 
-                  </Typography>
-                  <Typography id="Patient Information Description" sx={{ mt: 2 ,fontWeight: 'bold' }}>
-                    AGE: {age ? `${age}` : ''} 
-                  </Typography>
-                  <Typography id="Patient Information Description" sx={{ mt: 2 ,fontWeight: 'bold'}}>
-                    MOBILE: {mobile ? `${mobile}` : ''} 
-                  </Typography>
-                  <Typography id="Patient Information Description" sx={{ mt: 2 ,fontWeight: 'bold'}}>
-                    GENDER: {gender ? `${gender}` : ''} 
-                  </Typography>
-                  <Typography id="Successfull Message" sx={{ mt: 2 ,color: '#9c27b0', fontWeight: 'bold',fontSize: '20px',textAlign: 'center'}}  >
-                    UPDATE SUCCESSFULLY!!!
-                  </Typography>
-                </Box>
-              </Modal>
-            </Grid> 
-        </Grid>
-      </Box>
-  </Grid>
-   </Grid>
+      </Grid>   
   
   );
 }
