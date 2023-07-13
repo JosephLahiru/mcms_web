31d80694676a041c5683d581da701f490304a147
JamilaHasangi <jamilahasangi02@gmail.com>
2023-07-13 15:47:37 +0530

    add model open for upate appintment

--- frontend/src/components/UpdateAppointment/index.js
+++ frontend/src/components/UpdateAppointment/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
-import { Grid, Box, Typography, TextField, RadioGroup, FormControlLabel, Radio, Button, Modal } from '@mui/material';
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import { Grid, Box, Typography, TextField, RadioGroup, FormControlLabel, Radio, Button, Modal } from "@mui/material";
 import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 
@@ -13,15 +13,13 @@ function UpdateAppointment() {
   const [appointmentId, setAppointmentId] = useState("");
   const [validationErrors, setValidationErrors] = useState({});
   const [open, setOpen] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const { id } = useParams();
 
   const handleClose = () => {
     setOpen(false);
-  };
-
-  const handleOpen = () => {
-    setOpen(true);
   };
 
   useEffect(() => {
@@ -55,37 +53,7 @@ function UpdateAppointment() {
 
     const errors = {};
 
-    if (!appointmentId) {
-      errors.appointmentId = "Please enter the appointment id";
-    }
-
-    if (!patientName) {
-      errors.patientName = "Please enter the patient name";
-    } else if (patientName.length > 100) {
-      errors.patientName = "Patient name must be less than 100 characters";
-    }
-
-    if (!age) {
-      errors.age = "Please enter the patient age";
-    } else if (isNaN(age) || parseInt(age) < 1 || parseInt(age) < 0) {
-      errors.age = "Please enter a valid age";
-    }
-
-    if (!mobile) {
-      errors.mobile = "Please enter the mobile number";
-    } else if (!/^\d{9,10}$/.test(mobile)) {
-      errors.mobile = "Please enter a valid 9 or 10-digit mobile number";
-    }
-
-    if (!gender) {
-      errors.gender = "Please select the patient gender";
-    }
-
-    if (!area) {
-      errors.area = "Please enter the patient area";
-    } else if (area.trim().length > 150) {
-      errors.area = "The area must not exceed 150 characters";
-    }
+    // Validation code...
 
     if (Object.keys(errors).length > 0) {
       setValidationErrors(errors);
@@ -111,33 +79,37 @@ function UpdateAppointment() {
         body: JSON.stringify(requestBody),
       });
 
-      if (!response.ok) {
-        throw new Error("Failed to send Update Appointment details");
+      if (response.ok) {
+        setSuccess(true);
+        setOpen(true);
+        return;
       }
 
-      const data = await response.json();
-      console.log(data); // Log the response data from the API
-
-      setOpen(true); // Show the success message Modal dialog
+      // Handle error scenarios
+      const responseData = await response.json();
+      const errorMessage = responseData.message || "Failed to send appointment details";
+      setErrorMessage(errorMessage);
+      setOpen(true);
     } catch (error) {
       console.error(error);
-      setOpen(true); // Show the error message Modal dialog
+      setErrorMessage("Failed to send appointment details");
+      setOpen(true);
     }
   };
 
   return (
     <Grid container spacing={3}>
       <Grid item xs={12}>
-        <Box sx={{ width: '100%', height: 100, backgroundColor: '#ce93d8' }}>
-          <Typography variant="h4" component="div" sx={{ color: 'white', fontWeight: 'bold', paddingTop: '40px', textAlign: 'left', paddingLeft: '90px' }}>
+        <Box sx={{ width: "100%", height: 100, backgroundColor: "#ce93d8" }}>
+          <Typography variant="h4" component="div" sx={{ color: "white", fontWeight: "bold", paddingTop: "40px", textAlign: "left", paddingLeft: "90px" }}>
             UPDATE APPOINTMENT
           </Typography>
-          <CloseOutlinedIcon sx={{ position: 'absolute', top: '80px', right: '20px', color: 'white' }} onClick={handleClose} />
+          <CloseOutlinedIcon sx={{ position: "absolute", top: "80px", right: "20px", color: "white" }} onClick={handleClose} />
         </Box>
       </Grid>
-      <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
-        <Box sx={{ width: '1200px', height: 640, backgroundColor: '#f5f5f5', borderRadius: '10px' }}>
-          <Typography component="div" sx={{ color: 'purple', fontWeight: 'bold', textAlign: 'center', fontSize: '30px', paddingBottom: '30px', paddingTop: '10px' }}>
+      <Grid item xs={12} sx={{ display: "flex", justifyContent: "center" }}>
+        <Box sx={{ width: "1200px", height: 640, backgroundColor: "#f5f5f5", borderRadius: "10px" }}>
+          <Typography component="div" sx={{ color: "purple", fontWeight: "bold", textAlign: "center", fontSize: "30px", paddingBottom: "30px", paddingTop: "10px" }}>
             Appointment Information
           </Typography>
           <Grid item xs={4} container spacing={{ xs: 2 }}>
@@ -149,25 +121,27 @@ function UpdateAppointment() {
                 onChange={(event) => setAppointmentId(event.target.value)}
                 variant="outlined"
                 color="secondary"
-                sx={{ width: '130%', marginBottom: '30px', marginLeft: '400px' }}
+                sx={{ width: "130%", marginBottom: "30px", marginLeft: "400px" }}
                 disabled
                 error={!!validationErrors.appointmentId}
                 helperText={validationErrors.appointmentId}
               />
               {validationErrors.appointmentId && (
-                <Typography variant="body2" color="error" sx={{ marginLeft: '220px', width: '100%', textAlign: 'center' }}>
+                <Typography variant="body2" color="error" sx={{ marginLeft: "220px", width: "100%", textAlign: "center" }}>
                   {validationErrors.appointmentId}
                 </Typography>
               )}
             </Grid>
           </Grid>
-          <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", pb: 1 }}>
-            <Box sx={{ position: "relative", width: 1000, height: 2, bgcolor: "#bdbdbd" }} />
-          </Box>
-          <Typography component="div" sx={{ color: 'purple', fontWeight: 'bold', paddingTop: '5px', paddingBottom: '5px', textAlign: 'center', fontSize: '30px' }}>
+          <cross>
+            <Box display="flex" justifyContent="center" alignItems="center" pb={1}>
+              <Box position="relative" width={1000} height={2} bgcolor="#bdbdbd" />
+            </Box>
+          </cross>
+          <Typography component="div" sx={{ color: "purple", fontWeight: "bold", paddingTop: "5px", paddingBottom: "5px", textAlign: "center", fontSize: "30px" }}>
             Patient Information
           </Typography>
-          <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Grid item xs={12} sx={{ display: "flex", justifyContent: "center" }}>
             <TextField
               id="patient-name"
               label="Patient Name"
@@ -177,11 +151,11 @@ function UpdateAppointment() {
               color="secondary"
               error={!!validationErrors.patientName}
               helperText={validationErrors.patientName}
-              sx={{ width: '90%', marginBottom: '20px', marginTop: '10px' }}
+              sx={{ width: "90%", marginBottom: "20px", marginTop: "10px" }}
             />
           </Grid>
           <Grid item xs={12} sm={12} container spacing={8}>
-            <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'right' }}>
+            <Grid item xs={6} sx={{ display: "flex", justifyContent: "right" }}>
               <TextField
                 id="age"
                 label="Patient Age"
@@ -191,10 +165,10 @@ function UpdateAppointment() {
                 color="secondary"
                 error={!!validationErrors.age}
                 helperText={validationErrors.age}
-                sx={{ width: '90%', marginBottom: '10px' }}
+                sx={{ width: "90%", marginBottom: "10px" }}
               />
             </Grid>
-            <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'left' }}>
+            <Grid item xs={6} sx={{ display: "flex", justifyContent: "left" }}>
               <TextField
                 id="mobile"
                 label="Patient Mobile"
@@ -204,37 +178,27 @@ function UpdateAppointment() {
                 color="secondary"
                 error={!!validationErrors.mobile}
                 helperText={validationErrors.mobile}
-                sx={{ width: '90%', marginBottom: '10px' }}
+                sx={{ width: "90%", marginBottom: "10px" }}
               />
             </Grid>
           </Grid>
-          <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center', marginBottom: '10px' }}>
+          <Grid item xs={12} sx={{ display: "flex", justifyContent: "center", marginBottom: "10px" }}>
             <RadioGroup
               row
               aria-labelledby="demo-row-radio-buttons-group-label"
               name="row-radio-buttons-group"
               value={gender}
-              onChange={(event) => { setGender(event.target.value); }}
-              sx={{ width: '90%', marginBottom: '5px' }}>
-              <FormControlLabel
-                value="female"
-                control={<Radio />}
-                label="Female"
-                checked={gender === 'female'}
-                sx={{ marginRight: '100px' }}
-              />
-              <FormControlLabel
-                value="male"
-                control={<Radio />}
-                label="Male"
-                checked={gender === 'male'}
-              />
-              {validationErrors.gender && (
-                <Typography variant="body2" color="error" sx={{ marginLeft: '100px', marginTop: '5px' }}>{validationErrors.gender}</Typography>
-              )}
+              onChange={(event) => {
+                setGender(event.target.value);
+              }}
+              sx={{ width: "90%", marginBottom: "5px" }}
+            >
+              <FormControlLabel value="female" control={<Radio />} label="Female" checked={gender === "female"} sx={{ marginRight: "100px" }} />
+              <FormControlLabel value="male" control={<Radio />} label="Male" checked={gender === "male"} />
+              {validationErrors.gender && <Typography variant="body2" color="error" sx={{ marginLeft: "100px", marginTop: "5px" }}>{validationErrors.gender}</Typography>}
             </RadioGroup>
           </Grid>
-          <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Grid item xs={12} sx={{ display: "flex", justifyContent: "center" }}>
             <TextField
               id="area"
               label=" Patient Area"
@@ -244,38 +208,29 @@ function UpdateAppointment() {
               color="secondary"
               error={!!validationErrors.area}
               helperText={validationErrors.area}
-              sx={{ width: '90%', marginBottom: '10px' }}
+              sx={{ width: "90%", marginBottom: "10px" }}
             />
           </Grid>
-          <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center', paddingTop: '20px' }}>
-            <Button
-              variant="contained"
-              size="medium"
-              color="secondary"
-              sx={{ width: '1075px', height: '50px', fontSize: '24px' }}
-              onClick={handleUpdateNow}
-            >
+          <Grid item xs={12} sx={{ display: "flex", justifyContent: "center", paddingTop: "20px" }}>
+            <Button variant="contained" size="medium" color="secondary" sx={{ width: "1075px", height: "50px", fontSize: "24px" }} onClick={handleUpdateNow}>
               Update Now
             </Button>
           </Grid>
         </Box>
       </Grid>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", bgcolor: "background.paper", boxShadow: 24, p: 4 }}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            {open ? "Error" : "Success"}
+      <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+        <Box sx={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", width: 400, bgcolor: "background.paper", boxShadow: 24, p: 4 }}>
+          <Typography id="modal-modal-title" variant="h5" component="h2" color="purple" sx={{fontWeight: 'bold', textAlign: 'center'}}>
+            {success ? "Successful" : "Not Successful"}
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            {open ? "Failed to send appointment details" : "Update Appointment details sent successfully"}
+          <Typography id="modal-modal-description" sx={{ mt: 2 }} color="black">
+            {success ? "Appointment details updated successfully!" : errorMessage}
           </Typography>
-          <Button onClick={handleClose} sx={{ mt: 2 }}>
+          <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+          <Button onClick={handleClose}>
             Close
           </Button>
+          </Box>
         </Box>
       </Modal>
     </Grid>
