2e28acb01016825484419bf220081ce51cb549a3
PathumLakshitha <pathum_2019513@fot.ruh.ac.lk>
2023-07-21 14:54:55 +0530

    Create layout

--- frontend/src/components/GenerateBillNew/index.js
+++ frontend/src/components/GenerateBillNew/index.js
@@ -3,13 +3,13 @@ import 'react-toastify/dist/ReactToastify.css';
 import { 
   Grid,
   Paper,
-  InputLabel,
   TextField,
-  Select,
-  Typography,
-  MenuItem,
-  FormControl,
-  Button,
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell
 } from "@mui/material";
 import ViewStockForBill from "../ViewStockForBill";
 import { useAppstore } from './../../appStore';
@@ -20,19 +20,83 @@ function GenerateBillNew() {
 
 
     return (
-        <Grid container spacing={1} sx={{ width: dopen ? "calc(100% - 260px)" : "94%", marginLeft: dopen ? "250px" : "80px", marginTop: '40px', overflow: 'hidden', padding: '10px', transition: "width 0.7s ease" }}>
+        <Grid container spacing={1} sx={{ width: dopen ? "calc(100% - 260px)" : "94%", marginLeft: dopen ? "250px" : "80px", marginTop: '30px', overflow: 'hidden', padding: '10px', transition: "width 0.7s ease" }}>
             <Grid item xs={9}>
                 <Grid container spacing={1}>
                     <Grid item xs={6}>
                         <Paper style={{ padding: "10px" }}>
-                            <TextField size="small" label="Date" />
-                            <TextField size="small" label="Invoice no" />
+                            <TextField size="small" label="Date" variant="standard" style={{ marginBottom: "5px" }}/>
+                            <TextField size="small" label="Invoice no" variant="standard" />
                         </Paper>
                     </Grid>
                     <Grid item xs={6}>
                         <Paper style={{ padding: "10px" }}>
-                            <TextField size="small" label="Patient's Name" />
-                            <TextField size="small" label="Doctor's Name" />
+                            <TextField size="small" label="Patient's Name" variant="standard" style={{ marginBottom: "5px" }}/>
+                            <TextField size="small" label="Doctor's Name" variant="standard"/>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={12}>
+                    <Paper style={{ padding: "10px" }}>
+                        <Grid container spacing={1}>
+                            
+                            <Grid item xs={2}>
+                                <TextField size="small" label="Product Name" />
+                            </Grid>
+                            <Grid item xs={2}>
+                                <TextField size="small" label="Unit Price" />
+                            </Grid>
+                            <Grid item xs={2}>
+                                <TextField size="small" label="Quantity" />
+                            </Grid>
+                            <Grid item xs={2}>
+                                <TextField size="small" label="Discount" />
+                            </Grid>
+                            <Grid item xs={2}>
+                                <TextField size="small" label="Discount %" />
+                            </Grid>
+                            <Grid item xs={2}>
+                                <TextField size="small" label="Amount" />
+                            </Grid>
+                            <Grid item xs={12}>
+                            <TableContainer sx={{ maxHeight: 200, minHeight: 200 }} md={{ minWidth: 650 }} sm={{ minWidth: 650 }}>
+                            <Table>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Product</TableCell>
+                                        <TableCell>Expiry</TableCell>
+                                        <TableCell>Quantity</TableCell>
+                                        <TableCell>U/P</TableCell>
+                                        <TableCell>Discount</TableCell>
+                                        <TableCell>Amount</TableCell>
+                                                {/* Add more TableCell components for additional columns */}
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {/* Empty row to display no data available */}
+                                    <TableRow>
+                                        <TableCell colSpan={2}>No data available</TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table> 
+                            </TableContainer>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Paper style={{ padding: "10px" }}>
+                            <Grid container spacing={1}>
+                                <Grid item xs={6}>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <TextField size="small" label="Discount" style={{ marginBottom: "10px" }} />
+                                    <TextField size="small" label="$Total" />
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <TextField size="small" label="$Cash" style={{ marginBottom: "10px" }}/>
+                                    <TextField size="small" label="BAL" />
+                                </Grid>
+                            </Grid>
                         </Paper>
                     </Grid>
                 </Grid>
