93ddb61a73f6a2fd2a20204f3e812fbc3b9693f8
JamilaHasangi <jamilahasangi02@gmail.com>
2023-07-26 07:46:21 +0530

    edited filter details

--- frontend/src/components/ViewAppointment1/index.js
+++ frontend/src/components/ViewAppointment1/index.js
@@ -38,7 +38,7 @@ function ViewAppointment1() {
   const [appointment, setAppointment] = useState([]);
   const [filteredAppointment, setFilteredAppointment] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
-  const [filterOption, setFilterOption] = useState("Appointment Name");
+  const [filterOption, setFilterOption] = useState("Patient Name");
   const [confirmDialogOpen, setConfirmDialogOpen] = useState(false);
   const [itemToDelete, setItemToDelete] = useState(null);
   const [page, setPage] = useState(0);
@@ -126,7 +126,7 @@ const handleModalClose = () => {
   useEffect(() => {
     let results;
     switch (filterOption) {
-      case "Appointment Name":
+      case "Patient Name":
         if (searchTerm.length >= 1) {
           results = appointment.filter((item) => String(item.patient_name).includes(searchTerm));
         } else {
@@ -323,7 +323,7 @@ const handleModalClose = () => {
               label="Filter option"
               onChange={handleFilterChange}
             >
-                <MenuItem value="Appointment Name" >Appointment Name</MenuItem>
+                <MenuItem value="Patient Name" >Patient Name</MenuItem>
                 <MenuItem value="Appointment Number">Appointment Number</MenuItem>
                 <MenuItem value="Appointment Date">Appointment Date</MenuItem>
                 <MenuItem value="Appointment Id">Appointment Id</MenuItem>
